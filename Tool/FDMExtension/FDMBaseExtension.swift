56d194ec3862c52b00a1b9098663f2126412079e
发抖喵 <fa_dou_miao@163.com>
2020-07-07 11:12:18 +0800

    no message

--- Tool/FDMExtension/FDMBaseExtension.swift
+++ Tool/FDMExtension/FDMBaseExtension.swift
@@ -10,68 +10,77 @@ import UIKit
 
 //MARK: Base - CALayer
 extension CALayer {
-    func left() -> CGFloat {
-        return frame.origin.x
+    
+    var left: CGFloat {
+        get {
+            return frame.origin.x
+        }
+        set {
+            frame.origin.x = newValue
+        }
+    }
+
+    var top: CGFloat {
+        get {
+            return frame.origin.y
+        }
+        set {
+            frame.origin.y = newValue
+        }
     }
     
-    func left(_ x: CGFloat) {
-        frame.origin.x = x
+    var right: CGFloat {
+        get {
+            return frame.origin.x + frame.width
+        }
+        set {
+            frame.origin.x = newValue - frame.width
+        }
     }
     
-    func top() -> CGFloat {
-        return frame.origin.y
+    var bottom: CGFloat {
+        get {
+            return frame.origin.y + frame.height
+        }
+        set {
+            frame.origin.y = newValue - frame.height
+        }
     }
     
-    func top(_ y: CGFloat) {
-        frame.origin.y = y
+    var width: CGFloat {
+        get {
+            return frame.width
+        }
+        set {
+            frame.size.width = newValue
+        }
+    }
+
+    var height: CGFloat {
+        get {
+            return frame.height
+        }
+        set {
+            frame.size.height = newValue
+        }
     }
     
-    func right() -> CGFloat {
-        return frame.origin.x + frame.width
+    var origin: CGPoint {
+        get {
+            return frame.origin
+        }
+        set {
+            frame.origin = newValue
+        }
     }
     
-    func right(_ right: CGFloat) {
-        frame.origin.x = right - frame.width
-    }
-    
-    func bottom() -> CGFloat {
-        return frame.origin.y + frame.height
-    }
-    
-    func bottom(_ bottom: CGFloat) {
-        frame.origin.y = bottom - frame.height
-    }
-    
-    func width() -> CGFloat {
-        return frame.width
-    }
-    
-    func width(_ width: CGFloat) {
-        frame.size.width = width
-    }
-    
-    func height() -> CGFloat {
-        return frame.height
-    }
-    
-    func height(_ height: CGFloat) {
-        frame.size.height = height
-    }
-    
-    func origin() -> CGPoint {
-        return frame.origin
-    }
-    
-    func origin(_ origin: CGPoint) {
-        frame.origin = origin
-    }
-    
-    func size() -> CGSize {
-        return frame.size
-    }
-    
-    func size(_ size: CGSize) {
-        frame.size = size
+    var size: CGSize {
+        get {
+            return frame.size
+        }
+        set {
+            frame.size = newValue
+        }
     }
 }
 
@@ -99,84 +108,94 @@ extension UIView {
         return nil
     }
     
-    func left() -> CGFloat {
-        return frame.origin.x
+    var left: CGFloat {
+        get {
+            return frame.origin.x
+        }
+        set {
+            frame.origin.x = newValue
+        }
     }
     
-    func left(_ x: CGFloat) {
-        frame.origin.x = x
+    var top: CGFloat {
+        get {
+            return frame.origin.y
+        }
+        set {
+            frame.origin.y = newValue
+        }
     }
     
-    func top() -> CGFloat {
-        return frame.origin.y
+    var right: CGFloat {
+        get {
+            return frame.origin.x + frame.width
+        }
+        set {
+            frame.origin.x = newValue - frame.width
+        }
     }
     
-    func top(_ y: CGFloat) {
-        frame.origin.y = y
+    var bottom: CGFloat {
+        get {
+            return frame.origin.y + frame.height
+        }
+        set {
+            frame.origin.y = newValue - frame.height
+        }
     }
     
-    func right() -> CGFloat {
-        return frame.origin.x + frame.width
+    var width: CGFloat {
+        get {
+            return frame.width
+        }
+        set {
+            frame.size.width = newValue
+        }
+    }
+
+    var height: CGFloat {
+        get {
+            return frame.height
+        }
+        set {
+            frame.size.height = newValue
+        }
     }
     
-    func right(_ right: CGFloat) {
-        frame.origin.x = right - frame.width
+    var center_x: CGFloat {
+        get {
+            return center.x
+        }
+        set {
+            center.x = newValue
+        }
     }
     
-    func bottom() -> CGFloat {
-        return frame.origin.y + frame.height
+    var center_y: CGFloat {
+        get {
+            return center.y
+        }
+        set {
+            center.y = newValue
+        }
     }
     
-    func bottom(_ bottom: CGFloat) {
-        frame.origin.y = bottom - frame.height
+    var origin: CGPoint {
+        get {
+            return frame.origin
+        }
+        set {
+            frame.origin = newValue
+        }
     }
     
-    func width() -> CGFloat {
-        return frame.width
-    }
-    
-    func width(_ width: CGFloat) {
-        frame.size.width = width
-    }
-    
-    func height() -> CGFloat {
-        return frame.height
-    }
-    
-    func height(_ height: CGFloat) {
-        frame.size.height = height
-    }
-    
-    func centernX() -> CGFloat {
-        return center.x
-    }
-    
-    func centernX(_ x: CGFloat) {
-        center.x = x
-    }
-    
-    func centerY() -> CGFloat {
-        return center.y
-    }
-    
-    func centerY(_ y: CGFloat) {
-        center.y =  y
-    }
-    
-    func origin() -> CGPoint {
-        return frame.origin
-    }
-    
-    func origin(_ origin: CGPoint) {
-        frame.origin = origin
-    }
-    
-    func size() -> CGSize {
-        return frame.size
-    }
-    
-    func size(_ size: CGSize) {
-        frame.size = size
+    var size: CGSize {
+        get {
+            return frame.size
+        }
+        set {
+            frame.size = newValue
+        }
     }
 }
 
